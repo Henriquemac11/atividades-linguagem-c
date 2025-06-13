b1025cb3ded97e24e4edf1000d37d5a630abb5df
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-06-13 13:59:23 -0300

    Create Atividade1.c

---
+++ Atividade1.c
@@ -0,0 +1,17 @@
+//1. Crie um programa que calcule a soma de dois números digitados pelo usuário.
+#include <stdio.h>
+
+int main(){
+
+int n1 , n2 ;
+
+  printf("Qual o valor do primeiro número: \n");
+  scanf("%d", &n1);
+
+  printf("Qual o valor do segundo número: \n");
+  scanf("%d%", &n2);
+
+  int soma = n1 + n2
+  printf("A soma dos valores inseridos é: %d ", soma);
+
+}
